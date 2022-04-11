3db035978717beada9747a5dec92a61f9e11d333
Austin Wade <me@austinhunt.co>
2022-04-10 17:14:43 -0700

    update heading text and readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+The source code for [https://wires.dev](https://wires.dev).
--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hello, I'm a software engineer interested building delightful, futuristic technology.
+          👋 Hi, I'm a software engineer interested in building delightful user experiences and futuristic technology.
           You can contact me on{' '}
           <a href="https://discord.com/users/219219995471642635">Discord</a>,{' '}
           <a href="https://twitter.com/wiresdev">Twitter</a>, or{' '}
